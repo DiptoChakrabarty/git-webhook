e4cab64fd24cdedafcabdb8e6acfe00895ce2063
DiptoChakrabarty <diptochuck123@gmail.com>
2020-06-18 22:11:20 +0530

    webhook url

--- hook.py
+++ hook.py
@@ -2,11 +2,12 @@ from flask import Flask,json,request
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def home():
-    return "Welcome Guys"
+@app.route("/github",methods=["POST"])
+def git_post():
+    if request.headers["Content-Type"] == "application/json":
+        print(json.dumps(request.json))
+        return json.dumps(request.json)
 
 
 if  __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0")
-    
